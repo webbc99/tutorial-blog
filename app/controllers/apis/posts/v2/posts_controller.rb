333af0b28c2b0350b6d8ce87b52f34cfc07cbc65
Aerodyne <webbc99@gmail.com>
2022-02-07 12:52:24 +0000

    Changed title in v2 posts controller

--- app/controllers/apis/posts/v2/posts_controller.rb
+++ app/controllers/apis/posts/v2/posts_controller.rb
@@ -3,7 +3,7 @@ class Apis::Posts::V2::PostsController < ApplicationController
     @posts = []
     Post.all.each do |post|
       p = {
-        title: "TITLE",
+        title: post.title,
         content: post.content,
         user: User.find(post.user_id).name,
         topic: Topic.find(post.topic_id).name
