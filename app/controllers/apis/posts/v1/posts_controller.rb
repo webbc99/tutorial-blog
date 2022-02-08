026130ef7b096056c63e011e1c9aded19bdac02d
Aerodyne <webbc99@gmail.com>
2022-02-08 13:06:13 +0000

    Removed test v2 api and added latest_post

--- app/controllers/apis/posts/v1/posts_controller.rb
+++ app/controllers/apis/posts/v1/posts_controller.rb
@@ -1,15 +1,50 @@
 class Apis::Posts::V1::PostsController < ApplicationController
+  # def index
+  #   @posts = []
+  #   Post.all.each do |post|
+  #     p = {
+  #       title: post.title,
+  #       content: post.content,
+  #       user: User.find(post.user_id).name,
+  #       topic: Topic.find(post.topic_id).name
+  #     }
+  #     @posts << p
+  #   end
+  #   render json: @posts 
+  # end
+
+  # def latest_post
+  #   @post = Post.last
+  #   post = {
+  #     title: @post.title,
+  #     content: @post.content,
+  #     user: User.find(@post.user_id).name,
+  #     topic: Topic.find(@post.topic_id).name
+  #   }
+  #   render json: post
+  # end
+
+  def latest_post
+    render json: formatted_post(Post.last)
+  end
+
   def index
     @posts = []
     Post.all.each do |post|
-      p = {
-        title: post.title,
-        content: post.content,
-        user: User.find(post.user_id).name,
-        topic: Topic.find(post.topic_id).name
-      }
-      @posts << p
+      @posts << formatted_post(post)
     end
-    render json: @posts 
+    render json: @posts
+  end
+
+  private
+
+  def formatted_post(post)
+    formatted_post_data = {
+      title: post.title,
+      content: post.content,
+      user: User.find(post.user_id).name,
+      topic: Topic.find(post.topic_id).name
+    }
+    formatted_post_data
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'apis/posts/v1/posts#index'
+  root to: 'apis/posts/v1/posts#latest_post'
+  get 'posts', to: 'apis/posts/v1/posts#index'
 
   resources :users
   resources :comments
   resources :topics
   resources :posts
   post 'authenticate', to: 'authentication#authenticate'
+
   namespace :apis do
     namespace :posts do
       namespace :v1 do
         resources :posts, only: [:index]
-      end
-      namespace :v2 do
-        resources :posts, only: [:index]
+        get 'latest_post', to: 'posts#latest_post'
       end
     end
   end
